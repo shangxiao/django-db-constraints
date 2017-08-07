7d0d0d1cf814c9809180ae14e96c95b5102fee3d
David Sanders <dsanders@rapilabs.com>
2017-08-08 04:20:24 +1000

    Deal with modify existing constraints

--- django_db_constraints/apps.py
+++ django_db_constraints/apps.py
@@ -22,24 +22,38 @@ class AlterConstraints(ModelOptionOperation):
         state.reload_model(app_label, self.name_lower, delay=True)
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
-        new_model = to_state.apps.get_model(app_label, self.name)
-        if self.allow_migrate_model(schema_editor.connection.alias, new_model):
-            old_model = from_state.apps.get_model(app_label, self.name)
+        to_model = to_state.apps.get_model(app_label, self.name)
 
-            old_constraints = getattr(old_model._meta, self.option_name, {}).keys() - getattr(new_model._meta, self.option_name, {}).keys()
-            for constraint_name in old_constraints:
-                schema_editor.execute('ALTER TABLE {table} DROP CONSTRAINT {name}'.format(
-                    table=schema_editor.connection.ops.quote_name(new_model._meta.db_table),
-                    name=schema_editor.connection.ops.quote_name(constraint_name),
-                ))
+        if self.allow_migrate_model(schema_editor.connection.alias, to_model):
+            from_model = from_state.apps.get_model(app_label, self.name)
 
-            new_constraints = getattr(new_model._meta, self.option_name, {}).keys() - getattr(old_model._meta, self.option_name, {}).keys()
-            for constraint_name in new_constraints:
-                constraint = new_model._meta.db_constraints[constraint_name]
-                schema_editor.execute('ALTER TABLE {table} ADD CONSTRAINT {name} {constraint}'.format(
-                    table=schema_editor.connection.ops.quote_name(new_model._meta.db_table),
+            to_constraints = getattr(to_model._meta, self.option_name, {}).keys()
+            from_constraints = getattr(from_model._meta, self.option_name, {}).keys()
+
+            table_operations = tuple(
+                'DROP CONSTRAINT {name}'.format(
                     name=schema_editor.connection.ops.quote_name(constraint_name),
-                    constraint=constraint,
+                )
+                for constraint_name in from_constraints - to_constraints
+            ) + tuple(
+                'ADD CONSTRAINT {name} {constraint}'.format(
+                    name=schema_editor.connection.ops.quote_name(constraint_name),
+                    constraint=to_model._meta.db_constraints[constraint_name],
+                )
+                for constraint_name in to_constraints - from_constraints
+            ) + tuple(
+                'DROP CONSTRAINT {name}, ADD CONSTRAINT {name} {constraint}'.format(
+                    name=schema_editor.connection.ops.quote_name(constraint_name),
+                    constraint=to_model._meta.db_constraints[constraint_name],
+                )
+                for constraint_name in to_constraints & from_constraints
+                if to_model._meta.db_constraints[constraint_name] != from_model._meta.db_constraints[constraint_name]
+            )
+
+            if table_operations:
+                schema_editor.execute('ALTER TABLE {table} {table_operations}'.format(
+                    table=schema_editor.connection.ops.quote_name(to_model._meta.db_table),
+                    table_operations=', '.join(table_operations),
                 ))
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
