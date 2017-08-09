fc0171094078a0e1377bc8d80de1861b81f7dc78
David Sanders <dsanders@rapilabs.com>
2017-08-09 22:22:35 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='django-db-constraints',
-    version='0.1.0',
+    version='0.2.0',
     author='shangxiao',
     description='Add database table-level constraints to your Django model\'s Meta',
     long_description=readme(),
